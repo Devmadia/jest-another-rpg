427a1efdcf55246e2bcbd1f4e31fd0fa7b4bab97
Devmadia <agathaphan@icloud.com>
2020-07-20 22:44:41 -0500

    testing code for Potions.test.js

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,1 +1,11 @@
 const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    //  create new Potion objects with a property name of 'health'
+    const potion = new Potion('health');
+  
+    // takes a constructor as an argument
+    expect(potion.name).toBe('health');
+    // property that is a number of some kind
+    expect(potion.value).toEqual(expect.any(Number));
+  });
